7effc5434d6feb7d28dca4b6e87454f32c3f2906
fatma-douiri <76583464+fatma-douiri@users.noreply.github.com>
2021-02-18 14:03:34 +0100

    API Checkpoint Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
+import UserList from './Components/UserList/UserList'
+import {Card} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Card   border="primary"  >
+        <Card.Header style={{textAlign:'center'}}> <h1> Users List !</h1> </Card.Header>
+        <Card.Body>
+             <UserList/>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
